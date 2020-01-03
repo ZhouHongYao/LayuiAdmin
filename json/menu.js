37163848373d12ee1ac3dc9414545080954a55b3
z_hy <690711327@qq.com>
2020-01-03 11:19:26 +0800

    Update menu.js

--- json/menu.js
+++ json/menu.js
@@ -6,47 +6,6 @@
         "icon": "layui-icon-home",
         "href": "/"
     }, {
-        "title": "人力资源规划",
-        "icon": "layui-icon-unorderedlist",
-        "childs": [{
-            "title": "组织架构",
-            "href": "/resource/organization"
-        }, {
-            "title": "组织管理",
-            "href": "/resource/manage"
-        }, {
-            "title": "岗位管理",
-            "href": "/resource/postmanage"
-        }, {
-            "title": "编制管理",
-            "href": "/resource/plait"
-        }]
-    }, {
-        "title": "招聘与配置",
-        "icon": "layui-icon-unorderedlist",
-        "childs": [{
-            "title": "招聘申请",
-            "href": "/jobset/apply"
-        }, {
-            "title": "简历库管理",
-            "href": "/jobset/resume"
-        }, {
-            "title": "面试安排",
-            "href": "/jobset/audition"
-        }, {
-            "title": "录用",
-            "href": "/jobset/hire"
-        }, {
-            "title": "淘汰&流失",
-            "href": "/jobset/outflow"
-        }, {
-            "title": "招聘统计报表",
-            "href": "/jobset/coal"
-        },{
-          "title": "候选人",
-          "href": "/jobset/candidate"
-        }]
-    }, {
         "title": "详情页",
         "icon": "layui-icon-container",
         "childs": [{
